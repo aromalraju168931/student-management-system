01dedf214e222b65ee9526e094db13be98843352
aromal_raju <aromalkovilakathuparambil@gmail.com>
2026-06-10 23:08:21 +0530

    Forgot import in project level urls.py

--- backend/school_management_system/school_management_system/urls.py
+++ backend/school_management_system/school_management_system/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('student_management_module.urls'))
+    path('api/',include('student_management_module.urls')),
 
     #forcing django to serve imgs on render
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
