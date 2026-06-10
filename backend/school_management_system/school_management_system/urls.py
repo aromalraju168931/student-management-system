8629fee189a38d72679c1ca72b0845d8d2a1f874
aromal_raju <aromalkovilakathuparambil@gmail.com>
2026-06-10 23:04:22 +0530

    Forced production media serving

--- backend/school_management_system/school_management_system/urls.py
+++ backend/school_management_system/school_management_system/urls.py
@@ -22,4 +22,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('student_management_module.urls'))
+
+    #forcing django to serve imgs on render
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
